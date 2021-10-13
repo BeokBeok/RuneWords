70bec6cde04e2ff85c0fbc94019916a8fa45dd67
강현석 <kekemusa37@gmail.com>
2021-10-13 10:09:50 +0900

    :art: [적용] Dependency

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -1,4 +1,5 @@
 object ProjectConfig {
     const val GRADLE = "com.android.tools.build:gradle:7.0.2"
     const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31"
+    const val ANDROID_JUNIT5 = "de.mannodermaus.gradle.plugins:android-junit5:1.6.2.0"
 }
