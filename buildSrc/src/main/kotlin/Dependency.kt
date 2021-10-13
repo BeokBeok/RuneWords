beeef7f30ebb96cc238f6c08c6bdb37caf0804af
Hyeonseok Kang <kekemusa37@gmail.com>
2021-10-13 10:13:50 +0900

    buildSrc 적용 (#1)
    * :building_construction: [생성] buildSrc
    
    * :art: [적용] ProjectConfig
    
    * :art: [적용] Dependency
    
    * :white_check_mark: [적용] JUnit5

---
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -0,0 +1,21 @@
+object AndroidX {
+    const val CORE_KTX = "androidx.core:core-ktx:1.6.0"
+    const val APPCOMPAT =  "androidx.appcompat:appcompat:1.3.1"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.1"
+}
+
+object Google {
+    const val MATERIAL = "com.google.android.material:material:1.4.0"
+}
+
+object JUnit {
+    private const val VERSION = "5.7.0"
+
+    const val JUPITER_API = "org.junit.jupiter:junit-jupiter-api:$VERSION"
+    const val JUPITER_ENGINE = "org.junit.jupiter:junit-jupiter-engine:$VERSION"
+    const val JUPITER_PARAMS = "org.junit.jupiter:junit-jupiter-params:$VERSION"
+}
+
+object AssertJ {
+    const val CORE = "org.assertj:assertj-core:3.18.0"
+}
