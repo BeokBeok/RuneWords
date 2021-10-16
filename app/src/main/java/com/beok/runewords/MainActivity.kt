f00b777c076821f75e940fd9cfcbb5be5c195e94
Hyeonseok Kang <kekemusa37@gmail.com>
2021-10-16 18:57:34 +0900

    Compose 적용 (#6)
    * :heavy_plus_sign: compose
    
    * :lipstick: [적용] Compose
    
    * :fire: [제거] 불필요한 파일

--- app/src/main/java/com/beok/runewords/MainActivity.kt
+++ app/src/main/java/com/beok/runewords/MainActivity.kt
@@ -2,10 +2,15 @@ package com.beok.runewords
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            ActivityMainView.Layout()
+        }
     }
 }
--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -19,3 +19,14 @@ object JUnit {
 object AssertJ {
     const val CORE = "org.assertj:assertj-core:3.18.0"
 }
+
+object Compose {
+    const val VERSION = "1.0.4"
+
+    const val UI = "androidx.compose.ui:ui:$VERSION"
+    const val MATERIAL = "androidx.compose.material:material:$VERSION"
+    const val UI_TOOLING = "androidx.compose.ui:ui-tooling:$VERSION"
+    const val RUNTIME_LIVEDATA = "androidx.compose.runtime:runtime-livedata:$VERSION"
+    const val ACTIVITY = "androidx.activity:activity-compose:1.3.1"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout-compose:1.0.0-rc01"
+}
