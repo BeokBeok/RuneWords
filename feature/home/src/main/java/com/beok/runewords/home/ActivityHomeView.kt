ebeffe863481d4ec62a15178f0eaea2abbc13412
강현석 <kekemusa37@gmail.com>
2021-10-17 00:39:25 +0900

    :sparkles: [적용] 룬 리스트 화면 노출

--- feature/home/src/main/java/com/beok/runewords/home/ActivityHomeView.kt
+++ feature/home/src/main/java/com/beok/runewords/home/ActivityHomeView.kt
@@ -1,17 +1,50 @@
 package com.beok.runewords.home
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 object ActivityHomeView {
 
+    @ExperimentalFoundationApi
     @Composable
     fun Layout() {
         MaterialTheme {
-            Surface {
-                Text(text = "Hello World!")
+            LazyVerticalGrid(cells = GridCells.Adaptive(minSize = 128.dp)) {
+                items(Rune.all()) { item ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(20.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = item.iconResourceID),
+                            contentDescription = null,
+                            modifier = Modifier.width(50.dp).height(50.dp),
+                            contentScale = ContentScale.Crop
+                        )
+                        Text(text = stringResource(id = item.nameResourceID))
+                    }
+                }
             }
         }
     }
--- feature/home/src/main/java/com/beok/runewords/home/HomeActivity.kt
+++ feature/home/src/main/java/com/beok/runewords/home/HomeActivity.kt
@@ -4,10 +4,12 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ExperimentalFoundationApi
 
 @SuppressLint("CustomSplashScreen")
 class HomeActivity : AppCompatActivity() {
 
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
