3186beeeef791684c96333ed7eff611352db6bb9
강현석 <kekemusa37@gmail.com>
2021-10-16 23:03:49 +0900

    :sparkles: [구현] Rune enum

---
+++ feature/home/src/main/java/com/beok/runewords/home/Rune.kt
@@ -0,0 +1,39 @@
+package com.beok.runewords.home
+
+import androidx.annotation.DrawableRes
+
+enum class Rune(@DrawableRes val resourceID: Int) {
+    EL(resourceID = R.drawable.ic_rune_el),
+    ELD(resourceID = R.drawable.ic_rune_eld),
+    TIR(resourceID = R.drawable.ic_rune_tir),
+    NEF(resourceID = R.drawable.ic_rune_nef),
+    ETH(resourceID = R.drawable.ic_rune_eth),
+    ITH(resourceID = R.drawable.ic_rune_ith),
+    TAL(resourceID = R.drawable.ic_rune_tal),
+    RAL(resourceID = R.drawable.ic_rune_ral),
+    ORT(resourceID = R.drawable.ic_rune_ort),
+    THUL(resourceID = R.drawable.ic_rune_thul),
+    AMN(resourceID = R.drawable.ic_rune_amn),
+    SOL(resourceID = R.drawable.ic_rune_sol),
+    SHAEL(resourceID = R.drawable.ic_rune_shael),
+    DOL(resourceID = R.drawable.ic_rune_dol),
+    HEL(resourceID = R.drawable.ic_rune_hel),
+    IO(resourceID = R.drawable.ic_rune_io),
+    LUM(resourceID = R.drawable.ic_rune_lum),
+    KO(resourceID = R.drawable.ic_rune_ko),
+    FAL(resourceID = R.drawable.ic_rune_fal),
+    LEM(resourceID = R.drawable.ic_rune_lem),
+    PUL(resourceID = R.drawable.ic_rune_pul),
+    UM(resourceID = R.drawable.ic_rune_um),
+    MAL(resourceID = R.drawable.ic_rune_mal),
+    IST(resourceID = R.drawable.ic_rune_ist),
+    GUL(resourceID = R.drawable.ic_rune_gul),
+    VEX(resourceID = R.drawable.ic_rune_vex),
+    OHM(resourceID = R.drawable.ic_rune_ohm),
+    LO(resourceID = R.drawable.ic_rune_lo),
+    SUR(resourceID = R.drawable.ic_rune_sur),
+    BER(resourceID = R.drawable.ic_rune_ber),
+    JAH(resourceID = R.drawable.ic_rune_jah),
+    CHAM(resourceID = R.drawable.ic_rune_cham),
+    ZOD(resourceID = R.drawable.ic_rune_zod);
+}
