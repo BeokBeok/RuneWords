5982a96edc3652f570768b67d299d9396516bc82
강현석 <kekemusa37@gmail.com>
2021-10-16 23:26:40 +0900

    :sparkles: [적용] Rune enum에 문구 리소스 아이디

--- feature/home/src/main/java/com/beok/runewords/home/Rune.kt
+++ feature/home/src/main/java/com/beok/runewords/home/Rune.kt
@@ -1,39 +1,147 @@
 package com.beok.runewords.home
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
-enum class Rune(@DrawableRes val resourceID: Int) {
-    EL(resourceID = R.drawable.ic_rune_el),
-    ELD(resourceID = R.drawable.ic_rune_eld),
-    TIR(resourceID = R.drawable.ic_rune_tir),
-    NEF(resourceID = R.drawable.ic_rune_nef),
-    ETH(resourceID = R.drawable.ic_rune_eth),
-    ITH(resourceID = R.drawable.ic_rune_ith),
-    TAL(resourceID = R.drawable.ic_rune_tal),
-    RAL(resourceID = R.drawable.ic_rune_ral),
-    ORT(resourceID = R.drawable.ic_rune_ort),
-    THUL(resourceID = R.drawable.ic_rune_thul),
-    AMN(resourceID = R.drawable.ic_rune_amn),
-    SOL(resourceID = R.drawable.ic_rune_sol),
-    SHAEL(resourceID = R.drawable.ic_rune_shael),
-    DOL(resourceID = R.drawable.ic_rune_dol),
-    HEL(resourceID = R.drawable.ic_rune_hel),
-    IO(resourceID = R.drawable.ic_rune_io),
-    LUM(resourceID = R.drawable.ic_rune_lum),
-    KO(resourceID = R.drawable.ic_rune_ko),
-    FAL(resourceID = R.drawable.ic_rune_fal),
-    LEM(resourceID = R.drawable.ic_rune_lem),
-    PUL(resourceID = R.drawable.ic_rune_pul),
-    UM(resourceID = R.drawable.ic_rune_um),
-    MAL(resourceID = R.drawable.ic_rune_mal),
-    IST(resourceID = R.drawable.ic_rune_ist),
-    GUL(resourceID = R.drawable.ic_rune_gul),
-    VEX(resourceID = R.drawable.ic_rune_vex),
-    OHM(resourceID = R.drawable.ic_rune_ohm),
-    LO(resourceID = R.drawable.ic_rune_lo),
-    SUR(resourceID = R.drawable.ic_rune_sur),
-    BER(resourceID = R.drawable.ic_rune_ber),
-    JAH(resourceID = R.drawable.ic_rune_jah),
-    CHAM(resourceID = R.drawable.ic_rune_cham),
-    ZOD(resourceID = R.drawable.ic_rune_zod);
+enum class Rune(
+    @StringRes val nameResourceID: Int,
+    @DrawableRes val iconResourceID: Int
+) {
+    EL(
+        nameResourceID = R.string.title_rune_el,
+        iconResourceID = R.drawable.ic_rune_el
+    ),
+    ELD(
+        nameResourceID = R.string.title_rune_eld,
+        iconResourceID = R.drawable.ic_rune_eld
+    ),
+    TIR(
+        nameResourceID = R.string.title_rune_tir,
+        iconResourceID = R.drawable.ic_rune_tir
+    ),
+    NEF(
+        nameResourceID = R.string.title_rune_nef,
+        iconResourceID = R.drawable.ic_rune_nef
+    ),
+    ETH(
+        nameResourceID = R.string.title_rune_eth,
+        iconResourceID = R.drawable.ic_rune_eth
+    ),
+    ITH(
+        nameResourceID = R.string.title_rune_ith,
+        iconResourceID = R.drawable.ic_rune_ith
+    ),
+    TAL(
+        nameResourceID = R.string.title_rune_tal,
+        iconResourceID = R.drawable.ic_rune_tal
+    ),
+    RAL(
+        nameResourceID = R.string.title_rune_ral,
+        iconResourceID = R.drawable.ic_rune_ral
+    ),
+    ORT(
+        nameResourceID = R.string.title_rune_ort,
+        iconResourceID = R.drawable.ic_rune_ort
+    ),
+    THUL(
+        nameResourceID = R.string.title_rune_thul,
+        iconResourceID = R.drawable.ic_rune_thul
+    ),
+    AMN(
+        nameResourceID = R.string.title_rune_amn,
+        iconResourceID = R.drawable.ic_rune_amn
+    ),
+    SOL(
+        nameResourceID = R.string.title_rune_sol,
+        iconResourceID = R.drawable.ic_rune_sol
+    ),
+    SHAEL(
+        nameResourceID = R.string.title_rune_shael,
+        iconResourceID = R.drawable.ic_rune_shael
+    ),
+    DOL(
+        nameResourceID = R.string.title_rune_dol,
+        iconResourceID = R.drawable.ic_rune_dol
+    ),
+    HEL(
+        nameResourceID = R.string.title_rune_hel,
+        iconResourceID = R.drawable.ic_rune_hel
+    ),
+    IO(
+        nameResourceID = R.string.title_rune_io,
+        iconResourceID = R.drawable.ic_rune_io
+    ),
+    LUM(
+        nameResourceID = R.string.title_rune_lum,
+        iconResourceID = R.drawable.ic_rune_lum
+    ),
+    KO(
+        nameResourceID = R.string.title_rune_ko,
+        iconResourceID = R.drawable.ic_rune_ko
+    ),
+    FAL(
+        nameResourceID = R.string.title_rune_fal,
+        iconResourceID = R.drawable.ic_rune_fal
+    ),
+    LEM(
+        nameResourceID = R.string.title_rune_lem,
+        iconResourceID = R.drawable.ic_rune_lem
+    ),
+    PUL(
+        nameResourceID = R.string.title_rune_pul,
+        iconResourceID = R.drawable.ic_rune_pul
+    ),
+    UM(
+        nameResourceID = R.string.title_rune_um,
+        iconResourceID = R.drawable.ic_rune_um
+    ),
+    MAL(
+        nameResourceID = R.string.title_rune_mal,
+        iconResourceID = R.drawable.ic_rune_mal
+    ),
+    IST(
+        nameResourceID = R.string.title_rune_ist,
+        iconResourceID = R.drawable.ic_rune_ist
+    ),
+    GUL(
+        nameResourceID = R.string.title_rune_gul,
+        iconResourceID = R.drawable.ic_rune_gul
+    ),
+    VEX(
+        nameResourceID = R.string.title_rune_vex,
+        iconResourceID = R.drawable.ic_rune_vex
+    ),
+    OHM(
+        nameResourceID = R.string.title_rune_ohm,
+        iconResourceID = R.drawable.ic_rune_ohm
+    ),
+    LO(
+        nameResourceID = R.string.title_rune_lo,
+        iconResourceID = R.drawable.ic_rune_lo
+    ),
+    SUR(
+        nameResourceID = R.string.title_rune_sur,
+        iconResourceID = R.drawable.ic_rune_sur
+    ),
+    BER(
+        nameResourceID = R.string.title_rune_ber,
+        iconResourceID = R.drawable.ic_rune_ber
+    ),
+    JAH(
+        nameResourceID = R.string.title_rune_jah,
+        iconResourceID = R.drawable.ic_rune_jah
+    ),
+    CHAM(
+        nameResourceID = R.string.title_rune_cham,
+        iconResourceID = R.drawable.ic_rune_cham
+    ),
+    ZOD(
+        nameResourceID = R.string.title_rune_zod,
+        iconResourceID = R.drawable.ic_rune_zod
+    );
+
+    companion object {
+
+        fun all(): List<Rune> = values().toList()
+    }
 }
