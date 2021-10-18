94b865ecaaf7790cb318bb6ddbdd64b4f0e3f267
강현석 <kekemusa37@gmail.com>
2021-10-18 18:09:26 +0900

    :recycle: [정리] 불필요한 annotation warning ignore

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -14,25 +14,6 @@ android {
     }
 }
 
-tasks {
-// See https://kotlinlang.org/docs/reference/experimental.html#experimental-status-of-experimental-api(-markers)
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.freeCompilerArgs += listOf(
-            "-Xopt-in=kotlin.Experimental",
-            "-Xopt-in=kotlin.RequiresOptIn",
-            "-Xopt-in=kotlin.time.ExperimentalTime",
-            "-Xopt-in=kotlinx.coroutines.DelicateCoroutinesApi",
-            "-Xopt-in=androidx.compose.foundation.ExperimentalFoundationApi",
-            "-Xopt-in=coil.annotation.ExperimentalCoilApi",
-            "-Xuse-experimental=kotlin.ExperimentalStdlibApi",
-            "-Xuse-experimental=kotlinx.coroutines.FlowPreview",
-            "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi",
-            "-Xuse-experimental=kotlinx.coroutines.InternalCoroutinesApi",
-            "-Xuse-experimental=kotlinx.serialization.ExperimentalSerializationApi",
-        )
-    }
-}
-
 dependencies {
     implementation(project(":common"))
 
@@ -50,5 +31,14 @@ dependencies {
         implementation(RUNTIME_LIVEDATA)
         implementation(ACTIVITY)
         implementation(CONSTRAINT_LAYOUT)
+    }
+}
+
+tasks {
+// See https://kotlinlang.org/docs/reference/experimental.html#experimental-status-of-experimental-api(-markers)
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.freeCompilerArgs += listOf(
+            "-Xopt-in=androidx.compose.foundation.ExperimentalFoundationApi",
+        )
     }
 }
