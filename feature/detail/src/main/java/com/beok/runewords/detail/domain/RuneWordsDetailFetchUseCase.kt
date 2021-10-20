a8c2aab855ad16c73e2d4f163e92dd64a08e73ab
강현석 <kekemusa37@gmail.com>
2021-10-20 19:53:10 +0900

    :sparkles: [구현] RuneWordsDetailFetchUseCase

---
+++ feature/detail/src/main/java/com/beok/runewords/detail/domain/RuneWordsDetailFetchUseCase.kt
@@ -0,0 +1,19 @@
+package com.beok.runewords.detail.domain
+
+import com.beok.runewords.detail.domain.model.RuneWordsDetail
+
+interface RuneWordsDetailFetchUseCase {
+
+    suspend fun execute(name: String): Result<RuneWordsDetail>
+
+    class Fake : RuneWordsDetailFetchUseCase {
+
+        override suspend fun execute(name: String): Result<RuneWordsDetail> = runCatching {
+            RuneWordsDetail(
+                name = "call_to_arms",
+                runeCombination = listOf("amn", "ral", "mal", "ist", "ohm"),
+                type = "all_weapons"
+            )
+        }
+    }
+}
