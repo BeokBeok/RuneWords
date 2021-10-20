5c0929fdd0e844994b07ebf6edc5e22b72190fa4
강현석 <kekemusa37@gmail.com>
2021-10-20 17:59:44 +0900

    :sparkles: [구현] RuneWordsDetailRepository

---
+++ feature/detail/src/test/java/com/beok/runewords/detail/data/RuneWordsDetailRepositoryImplTest.kt
@@ -0,0 +1,25 @@
+package com.beok.runewords.detail.data
+
+import com.beok.runewords.detail.data.remote.RuneWordsDetailRemoteDataSource
+import com.beok.runewords.detail.domain.RuneWordsDetailRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class RuneWordsDetailRepositoryImplTest {
+
+    private val runeWordsDetailRemoteDataSource: RuneWordsDetailRemoteDataSource =
+        RuneWordsDetailRemoteDataSource.Fake()
+    private val repository: RuneWordsDetailRepository = RuneWordsDetailRepository.Fake()
+
+    @Test
+    fun `룬워드 정보를 불러옵니다`() = runBlocking {
+        val runeWordsName = "call_to_arms"
+        val expected = runeWordsDetailRemoteDataSource
+            .fetchInfo(name = runeWordsName)
+            .toDto()
+        val actual = repository.fetchInfo(name = runeWordsName)
+
+        assertEquals(expected, actual)
+    }
+}
