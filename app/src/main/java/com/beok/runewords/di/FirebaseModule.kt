d9d82e06e3fa08d225a19bc1d2f6f3f81540195e
강현석 <kekemusa37@gmail.com>
2021-10-25 18:47:02 +0900

    :sparkles: [구현] FirebaseAnalytics DI

--- app/src/main/java/com/beok/runewords/di/FirebaseModule.kt
+++ app/src/main/java/com/beok/runewords/di/FirebaseModule.kt
@@ -1,9 +1,12 @@
 package com.beok.runewords.di
 
+import android.content.Context
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -14,4 +17,10 @@ internal class FirebaseModule {
     @Provides
     @Singleton
     fun providesFireStore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun providesFirebaseAnalytics(
+        @ApplicationContext context: Context
+    ): FirebaseAnalytics = FirebaseAnalytics.getInstance(context)
 }
