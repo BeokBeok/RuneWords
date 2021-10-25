e6cc58f950e7814241e15924d430bb074cee6c74
강현석 <kekemusa37@gmail.com>
2021-10-25 19:47:05 +0900

    :chart_with_upwards_trend: [추가] 룬 리스트 클릭 시 이벤트 트래킹

--- feature/home/src/main/java/com/beok/runewords/home/HomeActivity.kt
+++ feature/home/src/main/java/com/beok/runewords/home/HomeActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.MobileAds
+import com.google.firebase.analytics.FirebaseAnalytics
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 internal class HomeActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var analytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         MobileAds.initialize(this)
         setContent {
-            ActivityHomeView.Layout(context = this)
+            ActivityHomeView.Layout(context = this, analytics = analytics)
         }
     }
 }
