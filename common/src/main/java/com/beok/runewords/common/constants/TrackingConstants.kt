57e44dca98dd746b2c48f19a9a9da86a6db3226d
Hyeonseok Kang <kekemusa37@gmail.com>
2021-10-25 19:51:52 +0900

    Firebase Analytics 연동 (#27)
    * :sparkles: [구현] FirebaseAnalytics DI
    
    * :sparkles: [연동] home 모듈에 hilt, firebase analytics
    
    * :fire: [제거] 불필요한 annotation
    
    * :chart_with_upwards_trend: [정의] TrackingConstants
    
    * :chart_with_upwards_trend: [추가] 룬 리스트 클릭 시 이벤트 트래킹
    
    * :chart_with_upwards_trend: [추가] 룬워드 클릭 시 이벤트 트래킹

---
+++ common/src/main/java/com/beok/runewords/common/constants/TrackingConstants.kt
@@ -0,0 +1,14 @@
+package com.beok.runewords.common.constants
+
+object TrackingConstants {
+
+    object Rune {
+        const val CLICK = "rune_click"
+        const val WORDS_WORDS = "rune_words_click"
+    }
+
+    object Params {
+        const val RUNE_NAME = "runeName"
+        const val RUNE_WORDS_NAME = "runeWordsName"
+    }
+}
--- feature/home/src/main/java/com/beok/runewords/home/HomeActivity.kt
+++ feature/home/src/main/java/com/beok/runewords/home/HomeActivity.kt
@@ -1,20 +1,25 @@
 package com.beok.runewords.home
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.MobileAds
+import com.google.firebase.analytics.FirebaseAnalytics
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
 internal class HomeActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var analytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         MobileAds.initialize(this)
         setContent {
-            ActivityHomeView.Layout(context = this)
+            ActivityHomeView.Layout(context = this, analytics = analytics)
         }
     }
 }
