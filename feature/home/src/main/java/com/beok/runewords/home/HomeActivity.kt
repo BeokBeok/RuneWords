2c4ddad3885d500ce91485faabce3f6d4f93d1a5
강현석 <kekemusa37@gmail.com>
2021-10-25 19:01:14 +0900

    :fire: [제거] 불필요한 annotation

--- feature/home/src/main/java/com/beok/runewords/home/HomeActivity.kt
+++ feature/home/src/main/java/com/beok/runewords/home/HomeActivity.kt
@@ -1,12 +1,10 @@
 package com.beok.runewords.home
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.MobileAds
 
-@SuppressLint("CustomSplashScreen")
 internal class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
