fe9327f6ef3ee6c273c589fed08dfbe213115e0b
강현석 <kekemusa37@gmail.com>
2021-11-11 10:27:32 +0900

    :rocket: [배포] 1.0.4

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,7 +1,7 @@
 object AndroidConfig {
     private const val MAJOR_VERSION = 1
     private const val MINOR_VERSION = 0
-    private const val PATCH_VERSION = 3
+    private const val PATCH_VERSION = 4
     const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
     const val VERSION_CODE = MAJOR_VERSION * 10_000 + MINOR_VERSION * 100 + PATCH_VERSION
 }
