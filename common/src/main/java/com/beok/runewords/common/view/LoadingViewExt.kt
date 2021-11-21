372e7205b5eb3aa0106dda62ce873a98337be34b
강현석 <kekemusa37@gmail.com>
2021-11-21 20:38:32 +0900

    :recycle: [개선] LiveData -> State (combination 모듈)

--- common/src/main/java/com/beok/runewords/common/view/LoadingViewExt.kt
+++ common/src/main/java/com/beok/runewords/common/view/LoadingViewExt.kt
@@ -5,16 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
-fun ContentLoading(isLoading: State<Boolean>) {
+fun ContentLoading(isLoading: Boolean) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        if (isLoading.value) CircularProgressIndicator(modifier = Modifier.wrapContentSize())
+        if (isLoading) CircularProgressIndicator(modifier = Modifier.wrapContentSize())
     }
 }
--- feature/combination/src/main/java/com/beok/runewords/combination/presenter/ActivityCombinationView.kt
+++ feature/combination/src/main/java/com/beok/runewords/combination/presenter/ActivityCombinationView.kt
@@ -4,23 +4,19 @@ import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -28,13 +24,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.os.bundleOf
+import com.beok.runewords.combination.domain.model.RuneWords
 import com.beok.runewords.common.BundleKeyConstants
-import com.beok.runewords.common.constants.TrackingConstants
 import com.beok.runewords.common.ext.resourceIDByName
 import com.beok.runewords.common.ext.startActivity
 import com.beok.runewords.common.model.Rune
 import com.beok.runewords.common.view.ContentLoading
-import com.google.firebase.analytics.FirebaseAnalytics
 
 internal object ActivityCombinationView {
 
@@ -44,15 +39,17 @@ internal object ActivityCombinationView {
     fun Layout(
         rune: Rune,
         context: Context,
-        viewModel: CombinationViewModel,
-        analytics: FirebaseAnalytics
+        isLoading: Boolean,
+        runeWords: List<RuneWords>,
+        runeWordClickTracking: (String) -> Unit
     ) {
         MaterialTheme {
             CombinationScaffold(
                 rune = rune,
                 context = context,
-                viewModel = viewModel,
-                analytics = analytics
+                isLoading = isLoading,
+                runeWords = runeWords,
+                runeWordClickTracking = runeWordClickTracking
             )
         }
     }
@@ -61,16 +58,21 @@ internal object ActivityCombinationView {
     private fun CombinationScaffold(
         rune: Rune,
         context: Context,
-        viewModel: CombinationViewModel,
-        analytics: FirebaseAnalytics
+        isLoading: Boolean,
+        runeWords: List<RuneWords>,
+        runeWordClickTracking: (String) -> Unit
     ) {
         Scaffold(
             topBar = {
                 CombinationTopBar(rune = rune)
             },
             content = {
-                ContentLoading(isLoading = viewModel.isLoading.observeAsState(initial = false))
-                CombinationContent(context = context, viewModel = viewModel, analytics = analytics)
+                ContentLoading(isLoading = isLoading)
+                CombinationContent(
+                    context = context,
+                    runeWords = runeWords,
+                    runeWordClickTracking = runeWordClickTracking
+                )
             }
         )
     }
@@ -78,12 +80,11 @@ internal object ActivityCombinationView {
     @Composable
     private fun CombinationContent(
         context: Context,
-        viewModel: CombinationViewModel,
-        analytics: FirebaseAnalytics
+        runeWords: List<RuneWords>,
+        runeWordClickTracking: (String) -> Unit
     ) {
-        val runeWords = viewModel.runeWordsGroup.observeAsState(initial = listOf())
         LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(runeWords.value) { item ->
+            items(runeWords) { item ->
                 if (context.resourceIDByName(item.name) == 0) return@items
                 val runeWordsName =
                     stringResource(id = context.resourceIDByName(item.name) ?: return@items)
@@ -97,10 +98,7 @@ internal object ActivityCombinationView {
                                     BundleKeyConstants.RUNE_WORDS_NAME to item.name
                                 )
                             )
-                            analytics.logEvent(
-                                TrackingConstants.Rune.WORDS_WORDS,
-                                bundleOf(TrackingConstants.Params.RUNE_WORDS_NAME to item.name)
-                            )
+                            runeWordClickTracking(item.name)
                         }
                         .padding(horizontal = 16.dp)
                         .padding(top = 16.dp),
