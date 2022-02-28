ca77d311f0bb7a8860cc7162f382f8dbd4700108
Hyeonseok Kang <kekemusa37@gmail.com>
2022-02-28 12:24:07 +0900

    즉시업데이트 기능 적용 (#58)
    * :sparkles: [적용] IMMEDIATE 업데이트
    
    * :fire: 업데이트 취소 토스트
    
    * :fire: [제거] FLEXIBLE 업데이트 시 사용되는 스낵바
    
    * :fire: [제거] FLEXIBLE 업데이트 시 사용하는 로직
    
    * :fire: [제거] FLEXIBLE 업데이트 시 사용하는 문구
    
    * :fire: [제거] 인앱업데이트 관련 이벤트 트래킹

--- feature/home/src/main/java/com/beok/runewords/home/HomeActivity.kt
+++ feature/home/src/main/java/com/beok/runewords/home/HomeActivity.kt
@@ -1,13 +1,9 @@
 package com.beok.runewords.home
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.core.os.bundleOf
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.beok.runewords.common.constants.TrackingConstants
@@ -18,8 +14,7 @@ import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
-import com.google.android.play.core.install.InstallStateUpdatedListener
-import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.AppUpdateType
 import com.google.firebase.analytics.FirebaseAnalytics
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -32,39 +27,18 @@ internal class HomeActivity : AppCompatActivity() {
 
     private val inAppUpdateViewModel by viewModels<InAppUpdateViewModel>()
 
-    private val installStateUpdatedListener by lazy {
-        InstallStateUpdatedListener {
-            if (it.installStatus() == InstallStatus.DOWNLOADED) {
-                analytics.logEvent(TrackingConstants.InAppUpdate.DOWNLOADED, bundleOf())
-                inAppUpdateViewModel.completeDownload()
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setupSplashScreen()
         setContent()
-        setupListener()
-        checkUpdatable()
         observeInAppUpdate()
     }
 
-    override fun onDestroy() {
-        inAppUpdateViewModel.unregisterInstallStateUpdatedListener(installStateUpdatedListener)
-        super.onDestroy()
-    }
+    override fun onResume() {
+        super.onResume()
 
-    @Suppress("Deprecation")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode != InAppUpdateViewModel.REQ_IN_APP_UPDATE) return
-        if (resultCode != Activity.RESULT_OK) {
-            Toast.makeText(this, getString(R.string.cancel_update), Toast.LENGTH_SHORT)
-                .show()
-        }
+        checkUpdatable()
     }
 
     private fun setupSplashScreen() {
@@ -85,10 +59,6 @@ internal class HomeActivity : AppCompatActivity() {
                         bundleOf(TrackingConstants.Params.RUNE_NAME to runeName)
                     )
                 },
-                inAppUpdateState = inAppUpdateViewModel.state
-                    .observeAsState(initial = InAppUpdateState.None)
-                    .value,
-                updateAction = inAppUpdateViewModel::installAndRestart
             )
         }
     }
@@ -97,27 +67,17 @@ internal class HomeActivity : AppCompatActivity() {
         inAppUpdateViewModel.checkAppUpdatable()
     }
 
-    private fun setupListener() {
-        inAppUpdateViewModel.registerInstallStateUpdatedListener(installStateUpdatedListener)
-    }
-
     private fun observeInAppUpdate() {
         inAppUpdateViewModel.state.observe(this) { state ->
             when (state) {
                 InAppUpdateState.None,
-                InAppUpdateState.Downloaded -> Unit
-                InAppUpdateState.Impossible -> {
-                    inAppUpdateViewModel
-                        .unregisterInstallStateUpdatedListener(installStateUpdatedListener)
-                }
+                InAppUpdateState.Impossible -> Unit
                 is InAppUpdateState.Possible -> {
-                    inAppUpdateViewModel.registerForHome(appUpdateInfo = state.info, target = this)
-                }
-                InAppUpdateState.Complete -> {
-                    analytics.logEvent(TrackingConstants.InAppUpdate.INSTALL, bundleOf())
-                    inAppUpdateViewModel.completeUpdate()
-                    inAppUpdateViewModel
-                        .unregisterInstallStateUpdatedListener(installStateUpdatedListener)
+                    inAppUpdateViewModel.registerForHome(
+                        appUpdateInfo = state.info,
+                        appUpdateType = AppUpdateType.IMMEDIATE,
+                        target = this
+                    )
                 }
             }.javaClass
         }
