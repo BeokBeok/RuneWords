1850d245c49b8e5bac9563588d0cc7a8fcb01445
Hyeonseok Kang <kekemusa37@gmail.com>
2022-02-28 12:24:07 +0900

    즉시업데이트 기능 적용 (#58)
    * :sparkles: [적용] IMMEDIATE 업데이트
    
    * :fire: 업데이트 취소 토스트
    
    * :fire: [제거] FLEXIBLE 업데이트 시 사용되는 스낵바
    
    * :fire: [제거] FLEXIBLE 업데이트 시 사용하는 로직
    
    * :fire: [제거] FLEXIBLE 업데이트 시 사용하는 문구
    
    * :fire: [제거] 인앱업데이트 관련 이벤트 트래킹

--- feature/home/src/main/java/com/beok/runewords/home/inapp/InAppUpdateViewModel.kt
+++ feature/home/src/main/java/com/beok/runewords/home/inapp/InAppUpdateViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.ViewModel
 import com.beok.runewords.home.HomeActivity
 import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
-import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.UpdateAvailability
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,19 +16,25 @@ internal class InAppUpdateViewModel @Inject constructor(
     private val inAppUpdateManager: AppUpdateManager
 ) : ViewModel() {
 
-    private var _state = MutableLiveData<InAppUpdateState>(InAppUpdateState.None)
+    private val _state = MutableLiveData<InAppUpdateState>(InAppUpdateState.None)
     val state: LiveData<InAppUpdateState> get() = _state
 
     fun checkAppUpdatable() {
         inAppUpdateManager.appUpdateInfo
             .addOnSuccessListener { appUpdateInfo ->
-                when {
+                _state.value = when {
+                    appUpdateInfo.updateAvailability() ==
+                        UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS -> {
+                            InAppUpdateState.Possible(info = appUpdateInfo)
+                        }
                     appUpdateInfo.updateAvailability() != UpdateAvailability.UPDATE_AVAILABLE -> {
-                        _state.value = InAppUpdateState.Impossible
+                        InAppUpdateState.Impossible
                     }
-                    appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE) -> {
-                        _state.value = InAppUpdateState.Possible(info = appUpdateInfo)
+                    appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE) ||
+                    appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE) -> {
+                        InAppUpdateState.Possible(info = appUpdateInfo)
                     }
+                    else -> InAppUpdateState.None
                 }
             }
     }
@@ -45,22 +50,6 @@ internal class InAppUpdateViewModel @Inject constructor(
             target,
             REQ_IN_APP_UPDATE
         )
-    }
-
-    fun registerInstallStateUpdatedListener(listener: InstallStateUpdatedListener) {
-        inAppUpdateManager.registerListener(listener)
-    }
-
-    fun unregisterInstallStateUpdatedListener(listener: InstallStateUpdatedListener) {
-        inAppUpdateManager.unregisterListener(listener)
-    }
-
-    fun completeDownload() {
-        _state.value = InAppUpdateState.Downloaded
-    }
-
-    fun installAndRestart() {
-        _state.value = InAppUpdateState.Complete
     }
 
     fun completeUpdate() {
