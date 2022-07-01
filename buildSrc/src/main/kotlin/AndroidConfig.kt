300bf99b5fcffb50152264d6cc9ca014d31cc068
BeokBeok <kekemusa37@gmail.com>
2022-07-01 20:08:41 +0900

    :rocket: [배포] 1.7.2

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,7 +1,7 @@
 object AndroidConfig {
     private const val MAJOR_VERSION = 1
     private const val MINOR_VERSION = 7
-    private const val PATCH_VERSION = 1
+    private const val PATCH_VERSION = 2
     const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
     const val VERSION_CODE = MAJOR_VERSION * 10_000 + MINOR_VERSION * 100 + PATCH_VERSION
 }
