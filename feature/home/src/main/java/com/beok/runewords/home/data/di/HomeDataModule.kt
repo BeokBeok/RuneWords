4fa144c61e600b4bc75942e62dc8159566a2481d
BeokBeok <kekemusa37@gmail.com>
2022-08-31 00:34:35 +0900

    [적용] RemoteConfig 설정

---
+++ feature/home/src/main/java/com/beok/runewords/home/data/di/HomeDataModule.kt
@@ -0,0 +1,25 @@
+package com.beok.runewords.home.data.di
+
+import com.beok.runewords.home.R
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HomeDataModule {
+
+    @Provides
+    @Singleton
+    fun providesRemoteConfig(): FirebaseRemoteConfig =
+        Firebase.remoteConfig.apply {
+            setDefaultsAsync(R.xml.remote_config_defaults)
+        }.also {
+            it.fetchAndActivate()
+        }
+}
