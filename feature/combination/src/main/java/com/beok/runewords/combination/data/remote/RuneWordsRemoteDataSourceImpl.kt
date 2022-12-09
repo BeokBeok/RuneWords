626c9c9664ef6d91933d177776507c8e5f93cb99
BeokBeok <kekemusa37@gmail.com>
2022-12-10 02:01:28 +0900

    [구현] 룬정보 아이콘 불러오는 함수 (data)

--- feature/combination/src/main/java/com/beok/runewords/combination/data/remote/RuneWordsRemoteDataSourceImpl.kt
+++ feature/combination/src/main/java/com/beok/runewords/combination/data/remote/RuneWordsRemoteDataSourceImpl.kt
@@ -3,12 +3,14 @@ package com.beok.runewords.combination.data.remote
 import com.beok.runewords.combination.data.entity.RuneWordsResponse
 import com.beok.runewords.common.ext.await
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 internal class RuneWordsRemoteDataSourceImpl @Inject constructor(
-    private val fireStore: FirebaseFirestore
+    private val fireStore: FirebaseFirestore,
+    private val remoteConfig: FirebaseRemoteConfig
 ) : RuneWordsRemoteDataSource {
 
     override suspend fun searchByRune(rune: String): List<RuneWordsResponse> =
@@ -25,8 +27,17 @@ internal class RuneWordsRemoteDataSourceImpl @Inject constructor(
             return@withContext runeWordGroup
         }
 
+    override suspend fun fetchRuneInfoIconType(): String = withContext(Dispatchers.IO) {
+        remoteConfig.fetchAndActivate()
+            .await()
+            .run {
+                remoteConfig.getString(KEY_RUNE_INFO_ICON_TYPE)
+            }
+    }
+
     companion object {
         private const val COLLECTION_NAME_RUNEWORDS = "runewords"
         private const val FIELD_NAME_RUNE_COMBINATION = "rune_combination"
+        private const val KEY_RUNE_INFO_ICON_TYPE = "key_rune_info_icon_type"
     }
 }
