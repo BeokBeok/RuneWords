55b9f1c8cbfd5577a00153a5ac8f954c87a33b10
강현석 <kekemusa37@gmail.com>
2023-04-07 14:59:46 +0900

    [구현] RuneWordsNavHost

---
+++ app/src/main/java/com/beok/runewords/navigation/RuneWordsNavHost.kt
@@ -0,0 +1,21 @@
+package com.beok.runewords.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.beok.runewords.home.presentation.navigation.homeNavigationRoute
+import com.beok.runewords.home.presentation.navigation.homeScreen
+
+@Composable
+fun RuneWordsNavHost(
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = homeNavigationRoute
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+    ) {
+        homeScreen()
+    }
+}
