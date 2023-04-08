2fa36a1b85721d960700944129f5b61fe0551917
강현석 <kekemusa37@gmail.com>
2023-04-09 01:31:08 +0900

    [개선] 불필요한 DI 로직 (firestore, analytics)

--- app/src/main/java/com/beok/runewords/di/FirebaseModule.kt
+++ app/src/main/java/com/beok/runewords/di/FirebaseModule.kt
@@ -1,10 +1,7 @@
 package com.beok.runewords.di
 
-import android.content.Context
 import com.beok.runewords.BuildConfig
 import com.beok.runewords.R
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
@@ -12,23 +9,12 @@ import com.google.firebase.remoteconfig.ktx.remoteConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal class FirebaseModule {
-
-    @Provides
-    @Singleton
-    fun providesFireStore(): FirebaseFirestore = FirebaseFirestore.getInstance()
-
-    @Provides
-    @Singleton
-    fun providesFirebaseAnalytics(
-        @ApplicationContext context: Context
-    ): FirebaseAnalytics = FirebaseAnalytics.getInstance(context)
 
     @Provides
     @Singleton
