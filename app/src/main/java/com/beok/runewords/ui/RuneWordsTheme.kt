322a04de7722cff87cf033429f387abfd0292bd7
강현석 <kekemusa37@gmail.com>
2023-04-10 23:18:19 +0900

    [구현] RuneWordsTheme

---
+++ app/src/main/java/com/beok/runewords/ui/RuneWordsTheme.kt
@@ -0,0 +1,32 @@
+package com.beok.runewords.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Color.White,
+    background = Color.Black
+)
+
+private val LightColorPalette = lightColors(
+    primary = Color.Black,
+    background = Color.White
+)
+
+@Composable
+fun RuneWordsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        content = content
+    )
+}
