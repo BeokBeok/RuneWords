48906c727e8c1aa6e2c8bb74c6d0affea5a72491
강현석 <kekemusa37@gmail.com>
2023-04-17 11:38:45 +0900

    [업데이트] gradle 버전

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.3.0")
+    implementation("com.android.tools.build:gradle:7.4.2")
     implementation(kotlin("gradle-plugin", "1.7.10"))
     implementation("com.squareup:javapoet:1.13.0")
 }
