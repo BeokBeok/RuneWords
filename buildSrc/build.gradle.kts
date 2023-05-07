11bbdb3451f1bbcc76b6163608f430961b8e3b70
강현석 <kekemusa37@gmail.com>
2023-05-07 14:03:22 +0900

    [업데이트] Compose, Kotlin 버전

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:7.4.2")
-    implementation(kotlin("gradle-plugin", "1.7.10"))
+    implementation(kotlin("gradle-plugin", "1.8.10"))
     implementation("com.squareup:javapoet:1.13.0")
 }
