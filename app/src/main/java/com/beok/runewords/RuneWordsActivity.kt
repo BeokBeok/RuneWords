174a1e0b0f1d724d467fecc357fd2e71d649b668
강현석 <kekemusa37@gmail.com>
2023-05-07 14:32:55 +0900

    [개선] PackageManager 버전 분기

--- app/src/main/java/com/beok/runewords/RuneWordsActivity.kt
+++ app/src/main/java/com/beok/runewords/RuneWordsActivity.kt
@@ -1,5 +1,7 @@
 package com.beok.runewords
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -88,9 +90,13 @@ internal class RuneWordsActivity : ComponentActivity() {
     }
 
     private fun refreshAppUpdateType() {
-        inAppUpdateViewModel.refreshAppUpdateType(
-            version = packageManager.getPackageInfo(packageName, 0).versionName
-        )
+        val packageManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            packageManager.getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(0L))
+        } else {
+            @Suppress("DEPRECATION")
+            packageManager.getPackageInfo(packageName, 0)
+        }
+        inAppUpdateViewModel.refreshAppUpdateType(version = packageManager.versionName)
     }
 
     private fun observeInAppUpdate() {
