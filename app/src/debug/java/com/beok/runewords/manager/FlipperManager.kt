ebbc688b026e23b4a6e8b844c7e4d3aae23a0e5f
강현석 <kekemusa37@gmail.com>
2023-05-29 17:18:36 +0900

    [구현] FlipperManager

---
+++ app/src/debug/java/com/beok/runewords/manager/FlipperManager.kt
@@ -0,0 +1,20 @@
+package com.beok.runewords.manager
+
+import android.content.Context
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
+
+class FlipperManager(private val context: Context) {
+
+    fun init() {
+        if (!FlipperUtils.shouldEnableFlipper(context)) return
+        SoLoader.init(context, false)
+        AndroidFlipperClient.getInstance(context)
+            .apply {
+                addPlugin(InspectorFlipperPlugin(context, DescriptorMapping.withDefaults()))
+            }.start()
+    }
+}
---
+++ app/src/release/java/com/beok/runewords/manager/FlipperManager.kt
@@ -0,0 +1,8 @@
+package com.beok.runewords.manager
+
+import android.content.Context
+
+class FlipperManager(private val context: Context) {
+
+    fun init() = Unit
+}
