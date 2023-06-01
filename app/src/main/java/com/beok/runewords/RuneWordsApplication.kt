3a63e2d1374b5fc63319f8c555a545a8731b2a6e
Hyeonseok Kang <kekemusa37@gmail.com>
2023-06-01 21:55:56 +0900

    FlipperManager 구현 (#145)
    * [이동] RuneWordsApplication을 main으로
    
    * [구현] FlipperManager
    
    * [적용] FlipperManager
    
    * [개선] FlipperManager를 object로

--- app/src/main/java/com/beok/runewords/RuneWordsApplication.kt
+++ app/src/main/java/com/beok/runewords/RuneWordsApplication.kt
@@ -1,8 +1,10 @@
 package com.beok.runewords
 
 import android.app.Application
+import com.beok.runewords.manager.FlipperManager
 import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class RuneWordsApplication : Application() {
@@ -11,5 +13,13 @@ class RuneWordsApplication : Application() {
         super.onCreate()
 
         MobileAds.initialize(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        if (BuildConfig.DEBUG) {
+            FlipperManager.init(context = this)
+        }
     }
 }
---
+++ app/src/release/java/com/beok/runewords/manager/FlipperManager.kt
@@ -0,0 +1,8 @@
+package com.beok.runewords.manager
+
+import android.content.Context
+
+object FlipperManager {
+
+    fun init(context: Context) = Unit
+}
