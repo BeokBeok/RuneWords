7d2dbe9c83a0aa7a518b1247bc45fe585b77f975
강현석 <kekemusa37@gmail.com>
2023-06-04 19:33:48 +0900

    [정리] 불필요한 코드

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,5 +1,4 @@
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
@@ -11,4 +10,5 @@ dependencyResolutionManagement {
     }
 }
 
+rootProject.name = "build-logic"
 include(":convention")
