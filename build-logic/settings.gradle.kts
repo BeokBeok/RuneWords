065c5dd10fef0788d7b30c9208fa89191a58eb15
강현석 <kekemusa37@gmail.com>
2023-06-04 14:24:04 +0900

    [구현] build-logic/settings.gradle.kts

---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,14 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/dependency.toml"))
+        }
+    }
+}
+
+include(":convention")
