7342f6c5e0f49e6e8672de201ee90bcac9121322
강현석 <kekemusa37@gmail.com>
2023-06-04 15:54:28 +0900

    [적용] Version Code 및 Name

--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -14,8 +14,20 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
 
             extensions.configure<ApplicationExtension> {
                 configureKotlinAndroid(this)
-                defaultConfig.targetSdk = 33
+                with(defaultConfig) {
+                    targetSdk = 33
+                    versionCode = VERSION_CODE
+                    versionName = VERSION_NAME
+                }
             }
         }
+    }
+
+    companion object {
+        private const val MAJOR_VERSION = 1
+        private const val MINOR_VERSION = 11
+        private const val PATCH_VERSION = 2
+        const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
+        const val VERSION_CODE = MAJOR_VERSION * 10_000 + MINOR_VERSION * 100 + PATCH_VERSION
     }
 }
