b69309d620a3d7ee6a9bccc14af40fc033f8c628
강현석 <kekemusa37@gmail.com>
2023-06-12 22:55:59 +0900

    :rocket:[배포] 1.12.0

--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -33,8 +33,8 @@ internal class AndroidApplicationConventionPlugin : Plugin<Project> {
 
     companion object {
         private const val MAJOR_VERSION = 1
-        private const val MINOR_VERSION = 11
-        private const val PATCH_VERSION = 2
+        private const val MINOR_VERSION = 12
+        private const val PATCH_VERSION = 0
         const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
         const val VERSION_CODE = MAJOR_VERSION * 10_000 + MINOR_VERSION * 100 + PATCH_VERSION
     }
