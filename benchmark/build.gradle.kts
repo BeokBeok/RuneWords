e8a329f669602a978d31bb3c09fdac3711310f38
강현석 <kekemusa37@gmail.com>
2023-06-20 23:40:41 +0900

    [이동] benchmark 관련 디펜던시

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -20,13 +20,6 @@ android {
     experimentalProperties["android.experimental.self-instrumenting"] = true
 }
 
-dependencies {
-    implementation("androidx.test.ext:junit:1.1.4")
-    implementation("androidx.test.espresso:espresso-core:3.5.0")
-    implementation("androidx.test.uiautomator:uiautomator:2.2.0")
-    implementation("androidx.benchmark:benchmark-macro-junit4:1.1.1")
-}
-
 androidComponents {
     beforeVariants(selector().all()) {
         it.enable = it.buildType == "benchmark"
--- build-logic/convention/src/main/java/AndroidBenchmarkConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidBenchmarkConventionPlugin.kt
@@ -3,7 +3,10 @@ import com.beok.runewords.convention.configureGradleManagedDevices
 import com.beok.runewords.convention.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 internal class AndroidBenchmarkConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -22,6 +25,15 @@ internal class AndroidBenchmarkConventionPlugin : Plugin<Project> {
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                 }
                 configureGradleManagedDevices(this)
+            }
+
+            val libs = extensions.getByType<VersionCatalogsExtension>()
+                .named("libs")
+            dependencies {
+                implementation(libs.findLibrary("junit").get())
+                implementation(libs.findLibrary("espresso.core").get())
+                implementation(libs.findLibrary("uiautomator").get())
+                implementation(libs.findLibrary("benchmark.macro.junit4").get())
             }
         }
     }
