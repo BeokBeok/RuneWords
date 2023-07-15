c9a3b51e7d25216305d944a3b3ce3556812420d2
Hyeonseok Kang <kekemusa37@gmail.com>
2023-07-15 21:47:36 +0900

    라이브러리 업데이트 (#174)
    * [업데이트] 라이브러리 버전
    
    * [정리] 미사용 코드

--- common/src/main/java/com/beok/runewords/common/ext/ContextExt.kt
+++ common/src/main/java/com/beok/runewords/common/ext/ContextExt.kt
@@ -1,23 +1,6 @@
 package com.beok.runewords.common.ext
 
 import android.content.Context
-import android.content.Intent
-import android.os.Bundle
-import androidx.core.os.bundleOf
-import timber.log.Timber
-
-fun Context.startActivity(
-    className: String,
-    bundle: Bundle = bundleOf(),
-    intentFlags: Int? = null
-) = runCatching {
-    startActivity(
-        Intent(this, Class.forName(className)).apply {
-            if (!bundle.isEmpty) putExtras(bundle)
-            if (intentFlags != null) flags = intentFlags
-        }
-    )
-}.onFailure(Timber::d)
 
 fun Context.resourceIDByName(name: String) = runCatching {
     resources.getIdentifier(name, "string", packageName)
