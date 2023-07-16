2b68e49149f44a99031509bc59b2f94ec2ce7202
강현석 <kekemusa37@gmail.com>
2023-07-17 00:05:25 +0900

    FlipperManager 코드 개선

--- app/src/release/java/com/beok/runewords/manager/FlipperManager.kt
+++ app/src/release/java/com/beok/runewords/manager/FlipperManager.kt
@@ -2,7 +2,7 @@ package com.beok.runewords.manager
 
 import android.content.Context
 
-class FlipperManager(private val context: Context) {
+class FlipperManager(context: Context) {
 
     fun init() = Unit
 }
