0eb2e3a4c617a993a13a8979b2a2ea914dd036da
Hyeonseok Kang <kekemusa37@gmail.com>
2023-08-17 23:11:59 +0900

    cache 적용 (#187)
    * [적용] 캐시 in 룬워드 상세 화면
    
    * [적용] 캐시 in 룬워드 조합 리스트 화면
    
    * [적용] 룬워드 상세 화면 캐시 업데이트 로직

--- feature/combination/src/main/java/com/beok/runewords/combination/di/RepositoryModule.kt
+++ feature/combination/src/main/java/com/beok/runewords/combination/di/RepositoryModule.kt
@@ -6,11 +6,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal abstract class RepositoryModule {
 
     @Binds
+    @Singleton
     abstract fun bindsRuneWordsRepository(impl: RuneWordsRepositoryImpl): RuneWordsRepository
 }
