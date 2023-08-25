baa2e915be276da020bc7456cb58808bbb1d71c9
강현석 <kekemusa37@gmail.com>
2023-08-25 21:57:54 +0900

    [적용] 룬워드 상세화면 진입 시 캐싱

--- feature/detail/src/main/java/com/beok/runewords/detail/data/RuneWordsDetailRepositoryImpl.kt
+++ feature/detail/src/main/java/com/beok/runewords/detail/data/RuneWordsDetailRepositoryImpl.kt
@@ -1,34 +1,39 @@
 package com.beok.runewords.detail.data
 
+import com.beok.runewords.common.util.toDomain
+import com.beok.runewords.detail.data.local.RuneWordsDetailDAO
+import com.beok.runewords.detail.data.model.RuneWordsDetailTable
 import com.beok.runewords.detail.data.remote.RuneWordsDetailRemoteDataSource
 import com.beok.runewords.detail.domain.RuneWordsDetailRepository
 import com.beok.runewords.detail.domain.model.RuneWordsDetail
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flow
 
 internal class RuneWordsDetailRepositoryImpl @Inject constructor(
+    private val localDataSource: RuneWordsDetailDAO,
     private val remoteDataSource: RuneWordsDetailRemoteDataSource
 ) : RuneWordsDetailRepository {
-    private val cache = mutableMapOf<String, RuneWordsDetail>()
 
     override fun fetchInfo(name: String): Flow<RuneWordsDetail> {
-        return flow {
-            cache.getOrPut(
-                key = name,
-                defaultValue = {
-                    remoteDataSource.fetchInfo(name).toDomain()
+        return localDataSource.findBy(name)
+            .flatMapConcat {
+                if (it.isNotEmpty()) {
+                    return@flatMapConcat it.toDomain().asFlow()
                 }
-            ).also { local ->
-                emit(local)
-            }.let { local ->
-                remoteDataSource.fetchInfo(name)
-                    .toDomain()
-                    .let { remote ->
-                        if (local == remote) return@flow
-                        cache[name] = remote
-                    }
+                flow {
+                    emit(
+                        remoteDataSource.fetchInfo(name)
+                            .toDomain()
+                            .also { runeWordsDetail ->
+                                localDataSource.insert(
+                                    RuneWordsDetailTable.fromDomain(runeWordsDetail)
+                                )
+                            }
+                    )
+                }
             }
-        }
     }
 }
