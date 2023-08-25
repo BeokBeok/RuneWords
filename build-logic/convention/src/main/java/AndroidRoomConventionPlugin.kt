a9f3163d821af2e38c06a4e9f2544720d439d2c7
강현석 <kekemusa37@gmail.com>
2023-08-25 22:08:11 +0900

    [수정] Room scheme warning

--- build-logic/convention/src/main/java/AndroidRoomConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidRoomConventionPlugin.kt
@@ -1,12 +1,25 @@
+import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 
 internal class AndroidRoomConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
+
+            extensions.configure<LibraryExtension> {
+                with(defaultConfig) {
+                    javaCompileOptions {
+                        annotationProcessorOptions {
+                            arguments["room.schemaLocation"] = "$projectDir/schemas"
+                        }
+                    }
+                }
+            }
+
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
                 implementation(libs.findLibrary("room.ktx").get())
