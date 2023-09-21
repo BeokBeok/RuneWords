a7ce6f3a69fdc79da52ea899ceb2b3bbe1313f21
강현석 <kekemusa37@gmail.com>
2023-09-22 00:27:02 +0900

    [적용] ksp in room

--- build-logic/convention/src/main/java/AndroidLibraryRoomConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryRoomConventionPlugin.kt
@@ -1,30 +1,39 @@
 import com.android.build.api.dsl.LibraryExtension
+import com.google.devtools.ksp.gradle.KspExtension
+import java.io.File
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.process.CommandLineArgumentProvider
 
 internal class AndroidLibraryRoomConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
+            pluginManager.apply("com.google.devtools.ksp")
 
-            extensions.configure<LibraryExtension> {
-                with(defaultConfig) {
-                    javaCompileOptions {
-                        annotationProcessorOptions {
-                            arguments["room.schemaLocation"] = "$projectDir/schemas"
-                        }
-                    }
-                }
+            extensions.configure<KspExtension> {
+                arg(RoomSchemaArgProvider(File(projectDir, "schemas")))
             }
 
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
                 implementation(libs.findLibrary("room.ktx").get())
-                kapt(libs.findLibrary("room.compiler").get())
+                ksp(libs.findLibrary("room.compiler").get())
             }
         }
+    }
+
+    class RoomSchemaArgProvider(
+        @get:InputDirectory
+        @get:PathSensitive(PathSensitivity.RELATIVE)
+        val schemaDir: File,
+    ) : CommandLineArgumentProvider {
+        override fun asArguments() = listOf("room.schemaLocation=${schemaDir.path}")
     }
 }
