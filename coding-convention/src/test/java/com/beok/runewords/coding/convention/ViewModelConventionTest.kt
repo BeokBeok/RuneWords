1a1fb30eaaf4d78022eeb5ce9658045ec4baa4e8
강현석 <kekemusa37@gmail.com>
2023-10-18 22:10:09 +0900

    [구현] ViewModelConventionTest

---
+++ coding-convention/src/test/java/com/beok/runewords/coding/convention/ViewModelConventionTest.kt
@@ -0,0 +1,27 @@
+package com.beok.runewords.coding.convention
+
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.declaration.KoClassDeclaration
+import com.lemonappdev.konsist.api.ext.list.withNameEndingWith
+import com.lemonappdev.konsist.api.verify.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class ViewModelConventionTest {
+    @Test
+    fun `ViewModel은 Test 코드가 있어야 한다`() {
+        Konsist.scopeFromProject()
+            .classes()
+            .withNameEndingWith("ViewModel")
+            .assertTrue(function = KoClassDeclaration::hasTestClass)
+    }
+
+    @Test
+    fun `ViewModel은 presentation 패키지에 있어야 한다`() {
+        Konsist.scopeFromProject()
+            .classes()
+            .withNameEndingWith("ViewModel")
+            .assertTrue {
+                it.resideInPackage("..presentation..")
+            }
+    }
+}
