504e95693428b4a45a3968e90bd71326caade4bc
강현석 <kekemusa37@gmail.com>
2024-03-07 21:46:52 +0900

    [업데이트] gradle 버전

--- build-logic/convention/src/main/java/config/KotlinAndroid.kt
+++ build-logic/convention/src/main/java/config/KotlinAndroid.kt
@@ -7,7 +7,7 @@ import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 internal fun Project.configureKotlinAndroid(
-    commonExtension: CommonExtension<*, *, *, *, *>
+    commonExtension: CommonExtension<*, *, *, *, *, *>
 ) {
     commonExtension.run {
         compileSdk = 34
