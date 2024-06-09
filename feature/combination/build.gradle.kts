c5ceaf1d8d6202eefe29b9ed4d496f0eb35920a3
Hyeonseok Kang <kekemusa37@gmail.com>
2024-06-09 21:15:22 +0900

    ComposeGuard 적용 (#355)

--- feature/combination/build.gradle.kts
+++ feature/combination/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.runewords.android.feature)
     alias(libs.plugins.runewords.android.library.room)
     alias(libs.plugins.runewords.android.kotlinx.serialization)
+    id("com.joetr.compose.guard")
 }
 
 android {
