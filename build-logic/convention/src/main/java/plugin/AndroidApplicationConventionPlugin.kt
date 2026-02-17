4f9c13f99b4c8bd42fd40bae9d9d2a064be3e0de
강현석 <kekemusa37@gmail.com>
2026-02-17 22:26:44 +0900

    3.0.2 버전을 배포합니다.

--- build-logic/convention/src/main/java/plugin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/plugin/AndroidApplicationConventionPlugin.kt
@@ -38,7 +38,7 @@ internal class AndroidApplicationConventionPlugin : Plugin<Project> {
     companion object {
         private const val MAJOR_VERSION = 3
         private const val MINOR_VERSION = 0
-        private const val PATCH_VERSION = 1
+        private const val PATCH_VERSION = 2
         const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
         const val VERSION_CODE = 11917
     }
