d1bd8bba604789632607f67ce3de7931643915c5
강현석 <kekemusa37@gmail.com>
2026-07-26 14:32:47 +0900

    fix: VERSION_CODE 11917로 롤백

--- build-logic/convention/src/main/java/plugin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/plugin/AndroidApplicationConventionPlugin.kt
@@ -40,6 +40,6 @@ internal class AndroidApplicationConventionPlugin : Plugin<Project> {
         private const val MINOR_VERSION = 2
         private const val PATCH_VERSION = 0
         const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
-        const val VERSION_CODE = 11918
+        const val VERSION_CODE = 11917
     }
 }
