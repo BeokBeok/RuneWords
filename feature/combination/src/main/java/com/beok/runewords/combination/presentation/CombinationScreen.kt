e34bba387080b54795ea65d7599ba8df3e60e6ea
Hyeonseok Kang <kekemusa37@gmail.com>
2026-08-23 14:24:49 +0900

    레더 전용 룬워드 정보를 정리합니다. (#436)

--- feature/combination/src/main/java/com/beok/runewords/combination/presentation/CombinationScreen.kt
+++ feature/combination/src/main/java/com/beok/runewords/combination/presentation/CombinationScreen.kt
@@ -84,18 +84,15 @@ private fun CombinationContent(
     LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(runeWords) { item ->
             if (context.resourceIDByName(item.name) < 1) return@items
-            var runeWordsName =
-                stringResource(id = context.resourceIDByName(item.name))
-            val isLadder = stringArrayResource(id = R.array.ladder_rune_words)
-                .contains(item.name)
             val isStandard = stringArrayResource(id = R.array.standard_rune_words)
                 .contains(item.name)
-            runeWordsName = if (isLadder) {
-                 stringResource(id = R.string.ladder_only, runeWordsName)
-            } else if (isStandard) {
-                stringResource(id = R.string.standard_only, runeWordsName)
+            val runeWordsName = if (isStandard) {
+                stringResource(
+                    id = R.string.standard_only,
+                    stringResource(id = context.resourceIDByName(item.name))
+                )
             } else {
-                runeWordsName
+                stringResource(id = context.resourceIDByName(item.name))
             }
 
             Column(
